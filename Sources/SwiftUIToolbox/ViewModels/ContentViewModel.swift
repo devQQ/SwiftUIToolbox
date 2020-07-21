573d3db375bd3a0c6e818bb0e9f1781cf8d0cc3f
Q Trang <devqapps@gmail.com>
2020-07-20 19:30:54 -0700

    updated properties content and height to public

--- Sources/SwiftUIToolbox/ViewModels/ContentViewModel.swift
+++ Sources/SwiftUIToolbox/ViewModels/ContentViewModel.swift
@@ -8,8 +8,8 @@
 import SwiftUI
 
 public struct ContentViewModel<Content: View> {
-    let content: Content
-    let height: CGFloat?
+    public let content: Content
+    public let height: CGFloat?
     
     public init(content: Content, height: CGFloat? = nil) {
         self.content = content
